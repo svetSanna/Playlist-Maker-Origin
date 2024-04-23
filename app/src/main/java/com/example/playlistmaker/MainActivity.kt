060f93cbdc2dd1aab3d8c2c7d696102d41b12581
svetSanna <154700819+svetSanna@users.noreply.github.com>
2024-04-23 23:32:10 +0500

    Revert "Спринт 8 - Макет и навигация"

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,44 +1,11 @@
 package com.example.playlistmaker
 
-import android.content.Intent
-import android.content.res.Configuration
-import android.os.Bundle
-import android.view.View
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import java.util.Locale
-
+import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val buttonSearch = findViewById<Button>(R.id.button_search)
-
-        val displayIntent = Intent(this, SearchActivity::class.java)
-        val buttonClickListener: View.OnClickListener = object : View.OnClickListener {
-            override fun onClick(v: View?) {
-                startActivity(displayIntent)
-                //Toast.makeText(this@MainActivity, "Нажали на кнопку \"Поиск\"!", Toast.LENGTH_SHORT).show()
-            }
-        }
-        buttonSearch.setOnClickListener(buttonClickListener)
-
-        val buttonMedia = findViewById<Button>(R.id.button_media)
-
-        buttonMedia.setOnClickListener {
-            val displayIntent = Intent(this, MediaActivity::class.java)
-            startActivity(displayIntent)
-            // Toast.makeText(this@MainActivity, "Нажали на кнопку \"Медиатека\"!", Toast.LENGTH_SHORT).show()
-        }
-
-        val buttonSettings = findViewById<Button>(R.id.button_settings)
-
-        buttonSettings.setOnClickListener {
-            val displayIntent = Intent(this, SettingsActivity::class.java)
-            startActivity(displayIntent)
-            //Toast.makeText(this@MainActivity, "Нажали на кнопку \"Настройки\"!", Toast.LENGTH_SHORT).show()
-        }
     }
 }
