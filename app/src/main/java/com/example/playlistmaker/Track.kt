ce6f37524072775daa560e43faf3c8b41e5f0bb6
Svetlana <s.a.novakovskaya@yandex.ru>
2024-05-06 22:22:14 +0500

    Спринт 10. Создала класс Track - для хранения информации о треке

---
+++ app/src/main/java/com/example/playlistmaker/Track.kt
@@ -0,0 +1,10 @@
+package com.example.playlistmaker
+
+
+// Класс для хранения информации об одном треке
+class Track (
+    var trackName: String, // Название композиции
+    var artistName: String, // Имя исполнителя
+    var trackTime: String, // Продолжительность трека
+    var artworkUrl100: String // Ссылка на изображение обложки
+)
